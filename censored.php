b51ca96f66c03407a3b13e077bcbd03e5af2db96
reistence <gvlamberti@gmail.com>
2022-11-24 15:33:26 +0100

    ex completed

--- censored.php
+++ censored.php
@@ -1,12 +1,39 @@
+<?php
+    $user_txt = $_GET['txt'];
+    // var_dump($user_txt);
+    $user_word = $_GET['word'];
+    $censored_txt = str_ireplace($user_word, '***', $user_txt );
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="css/style.css">
+
     <title>Censored Words</title>
 </head>
 <body>
+<div class="container">
+
+    <h1>Censored</h1>
+
+    <div class="result-container">
+
+        
+        <h3>The original Text was:</h3>
+        <p><?php echo $user_txt ?></p>
+        <p>The original text is <?php echo strlen($user_txt) ?> words long</p>
+        
+        <h3>The censored one is:</h3>
+        <p> <?php echo $censored_txt ?> </p>
+        <p>The censored text is <?php echo strlen($user_txt) ?> words long </p>
+
+    </div>
+</div>
     
 </body>
 </html>
--- index.php
+++ index.php
@@ -1,25 +1,37 @@
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
     <title>PHP Badwords</title>
 </head>
 <body>
-    <h1>Censor</h1>
+    <div class="container">
+        <h1>Censor</h1>
+    
+        <form action="censored.php" method="get">
+            <div>
+                <label for="txt">Text:</label>
+                <textarea type="text" name="txt" id="txt" placeholder="Insert a text" cols="40" rows="5"></textarea>
 
+            </div>
+            
 
-    <form action="censored.php" method="get">
-        <label for="txt">Text:</label>
-        <input type="text" placeholder="Insert a text" name="txt" id="txt" >
-         <br>
-         <label for="word">Word:</label>
-        <input type="text" placeholder="Insert the word you'd like to censor" name="word" id="word">
-        <br>
-        <button type="submit">Submit</button>
-        <button type="reset">Reset</button>
-    </form>
+             <div>
+                 <label for="word">Word:</label>
+                <input type="text" placeholder="Insert the word you'd like to censor" name="word" id="word">
+
+             </div>
+            
+            <button type="submit">Submit</button>
+            <button type="reset">Reset</button>
+        </form>
+    </div>
+
+    <!-- Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni, molestias repudiandae iste ipsum quaerat nihil rerum veniam temporibus inventore tempore. -->
 
 </body>
 </html>
